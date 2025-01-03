cd716c94767c17ea18fb413a67d0d2f498a78168
bryan0060 <ojalaifood@gmail.com>
2025-01-02 20:10:06 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EasySign',
+  webDir: 'www'
+};
+
+export default config;
